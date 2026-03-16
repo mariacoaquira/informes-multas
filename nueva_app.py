e6bdde565e7e60556a8c529e3f47658d0d24fcc3
mariacoaquira <maria.coaquira.ch@gmail.com>
2026-03-16 09:45:18 -0500

    Add files via upload

--- nueva_app.py
+++ nueva_app.py
@@ -1,5 +1,5 @@
 import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
+#ssl._create_default_https_context = ssl._create_unverified_context
 import streamlit as st
 import io
 import locale
